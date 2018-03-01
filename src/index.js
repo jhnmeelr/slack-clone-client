370beca75fe21003806299210bc55c780f4eb52d
jm <jhn.meelr@gmail.com>
2018-03-01 09:17:42 +0200

    Sending, Verifying, and Refreshing JWT Tokens in Node.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import { HttpLink } from 'apollo-link-http';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'semantic-ui-css/semantic.min.css';
 
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
+const httpLink = createHttpLink({ uri: 'http://localhost:8080/graphql' });
+
+const middlewareLink = setContext(() => ({
+  headers: {
+    'x-token': localStorage.getItem('token'),
+    'x-refresh-token': localStorage.getItem('refreshToken'),
+  },
+}));
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+
+  if (headers) {
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken);
+    }
+  }
+
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:8080/graphql' }),
+  link: afterwareLink.concat(middlewareLink.concat(httpLink)),
   cache: new InMemoryCache(),
 });
 
